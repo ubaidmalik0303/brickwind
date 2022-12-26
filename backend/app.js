1f46b4c7a1c8f816013d21611d6d136b726731f3
ubaidmalik0303 <ubaidmalik0303@gmail.com>
2022-12-26 13:34:30 +0500

    path error solved

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const errorMiddleware = require("./middleware/error");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
+const path = require("path");
 
 // Config
 if (process.env.NODE_ENV !== "PRODUCTION") {
